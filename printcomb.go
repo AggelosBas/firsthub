b6151d591b0f2e00fd42e7d89a2f62d2ee23eb8f
abassili <aggelosbassili@gmail.com>
2025-05-07 12:51:46 -0400

    my first go program comb

--- printcomb.go
+++ printcomb.go
@@ -9,7 +9,7 @@ func PrintComb() {
                 z01.PrintRune(rune('0' + i))
                 z01.PrintRune(rune('0' + j))
                 z01.PrintRune(rune('0' + k))
-                if i != 7  j != 8  k != 9 {
+                if i != 7 || j != 8 || k != 9 {
                     z01.PrintRune(',')
                     z01.PrintRune(' ')
 
