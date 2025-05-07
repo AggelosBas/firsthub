f5b89241d78ab469c8e5c86df1675e3cba538df8
abassili <aggelosbassili@gmail.com>
2025-05-07 12:16:49 -0400

    my first go program print comb

--- printcomb.go
+++ printcomb.go
@@ -3,19 +3,18 @@ package main
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-    for i := 0; i <= 7; i++ {
-        for j := i + 1; j <= 8; j++ {
-            for k := j + 1; k <= 9; k++ {
-                if i != 9 || j != 8 || k != 9 {
-                    fmt.Printf("%d%d%d, ", i, j, k)
-                } else {
-                    fmt.Printf("%d%d%d", i, j, k)
-                }
-            }
-        }
-    }
-}
-
-func main() {
-    PrintComb()
+	for i := 0; i <= 7; i++{
+		for j := i + 1; j <= 8; j++{
+			for k := j + 1; k<= 9; k++{
+				z01.PrintRune(rune('0' + i))
+				z01.PrintRune(rune('0' + j))
+				z01.PrintRune(rune('0' + k))
+				if i!=7 || j !=8 || k !=9 {
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
+				}
+			}
+		}
+	}
+	z01.PrintRune('\n')
 }
