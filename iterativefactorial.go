fb79ab58d8a5f5c17727243ba9404cd7068ca6f8
Aggelos Bassili <abassili@noreply.platform.zone01.gr>
2025-05-12 10:59:09 +0000

    Update iterativefactorial.go

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -3,16 +3,16 @@ package student
 func IterativeFactorial(nb int) int {
 	if nb < 0 {
 		return 0
-	}
+		}
 
 	result := 1
-
-	for i := 1; i <= nb; i++ {
+    for i := 1; i <= nb; i++ {
 		result *= i
 
 		if result < 0 {
 			return 0
-			}
+		}
 	}
+
 	return result
 }
