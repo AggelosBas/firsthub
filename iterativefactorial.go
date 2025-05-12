37561c84f962b4ba577367336be4376694bf897b
gargyros <georgemarkos2@gmail.com>
2025-05-12 14:31:43 +0300

    iterativefactorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -3,10 +3,10 @@ package student
 func IterativeFactorial(nb int) int {
 	if nb < 0 {
 		return 0
-		}
+	}
 
 	result := 1
-    for i := 1; i <= nb; i++ {
+	for i := 1; i <= nb; i++ {
 		result *= i
 
 		if result < 0 {
