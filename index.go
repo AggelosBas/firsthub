911303deec2f1708a78345f717f0aa32543009dc
abassili <aggelosbassili@gmail.com>
2025-05-14 20:36:52 +0300

    index

---
+++ index.go
@@ -0,0 +1,17 @@
+package piscine
+
+func Index(s string, toFind string) int {
+	sLen := len(s)
+	toFindLen := len(toFind)
+
+	if toFindLen == 0 || toFindLen > sLen {
+		return -1
+	}
+
+	for i := 0; i <= sLen-toFindLen; i++ {
+		if s[i:i+toFindLen] == toFind {
+			return i
+		}
+	}
+	return -1
+}
