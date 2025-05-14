525fdf825fd1051f9d1c861446f97cd292f80821
abassili <aggelosbassili@gmail.com>
2025-05-14 21:55:52 +0300

    isalpha

--- isalpha.go
+++ isalpha.go
@@ -2,7 +2,7 @@ package student
 
 func IsAlpha(s string) bool {
 	for i := 0; i < len(s); i++ {
-		if !((s[i] >= '0' && s[i] <= '9') || (s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z')) || s[i] == '' {
+		if !((s[i] >= '0' && s[i] <= '9') || (s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z')) || s[i] == ' ' {
 			return false
 		}
 	}
