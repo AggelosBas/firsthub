b972806958df3348c8900c98e84de249afa7d4d6
abassili <aggelosbassili@gmail.com>
2025-05-14 21:01:51 +0300

    index

--- index.go
+++ index.go
@@ -1,12 +1,12 @@
 package student
 
 func Index(s string, toFind string) int {
-	sLen := len(toFind)
-	if a == 0{
+	a := len(toFind)
+	if a == 0 {
 		return 0
 	}
-		for i := 0; i <= sLen-toFindLen; i++ {
-		if s[i:i+toFindLen] == toFind {
+	for i := 0; i <= len(s)-a; i++ {
+		if s[i:i+a] == toFind {
 			return i
 		}
 	}
