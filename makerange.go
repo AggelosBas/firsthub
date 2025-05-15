b570ff06af65bff870ce60ae915ee7833adcfa6f
abassili <aggelosbassili@gmail.com>
2025-05-15 15:51:37 +0300

    makerange

---
+++ makerange.go
@@ -0,0 +1,13 @@
+package student
+
+func MakeRange(min, max int) []int {
+	if min >= max {
+		return nil
+	}
+
+	var result []int
+	for i := min; i < max; i++ {
+		result = append(result, i)
+	}
+	return result
+}
