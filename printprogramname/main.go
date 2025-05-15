987d5f237d17de1de305d00e32dd253e94a0b048
abassili <aggelosbassili@gmail.com>
2025-05-15 13:51:15 +0300

    printprogramname

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -1,11 +1,30 @@
 package main
 
 import (
-	"fmt"
 	"os"
-	"path/filepath"
+
+	"github.com/01-edu/z01"
 )
 
 func main() {
-	fmt.Println(filepath.Base(os.Args[0]))
+	arg := os.Args[0]
+
+	// Βρίσκουμε το τελευταίο '/' για να πάρουμε το basename
+	lastSlash := -1
+	for i := 0; i < len(arg); i++ {
+		if arg[i] == '/' {
+			lastSlash = i
+		}
+	}
+
+	name := arg
+	if lastSlash != -1 {
+		name = arg[lastSlash+1:]
+	}
+
+	// Εκτυπώνουμε χαρακτήρα-χαρακτήρα
+	for _, c := range name {
+		z01.PrintRune(c)
+	}
+	z01.PrintRune('\n')
 }
