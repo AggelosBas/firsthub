4574442a5f41dbea2720fa54d730d9a13e710626
abassili <aggelosbassili@gmail.com>
2025-05-15 16:05:54 +0300

    splitwhitespaces

---
+++ splitwhitespaces.go
@@ -0,0 +1,23 @@
+package student
+
+func SplitWhiteSpaces(s string) []string {
+	words := []string{}
+	word := ""
+
+	for _, ch := range s {
+		if ch == ' ' || ch == '\t' || ch == '\n' {
+			if word != "" {
+				words = append(words, word)
+				word = ""
+			}
+		} else {
+			word += string(ch)
+		}
+	}
+
+	if word != "" {
+		words = append(words, word)
+	}
+
+	return words
+}
