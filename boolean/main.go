3190a2018004581de7af8f93b3e26efd07f6ba4b
abassili <aggelosbassili@gmail.com>
2025-05-20 01:57:47 +0300

    boolean

--- boolean/main.go
+++ boolean/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	
 	"os"
 	
 	"github.com/01-edu/z01"
@@ -9,14 +8,12 @@ import (
 
 func printStr(s string) {
 	for _, r := range s {
-		
 		z01.PrintRune(r)
 	}
 	z01.PrintRune('\n')
 }
 
 func isEven(nbr int) bool {
-	
 	return nbr%2 == 0
 }
 
