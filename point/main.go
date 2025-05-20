35534c7fd34cb8219b2c3bd55815cf9da0f6565b
abassili <aggelosbassili@gmail.com>
2025-05-20 16:02:35 +0300

    point

---
+++ point/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "github.com/01-edu/z01"
+
+type point struct {
+	x int
+	y int
+}
+
+func setPoint(ptr *point) {
+	ptr.x = 42
+	ptr.y = 21
+}
+
+func printNbr(n int) {
+	if n < 0 {
+		z01.PrintRune('-')
+		n = -n
+	}
+	if n >= 10 {
+		printNbr(n / 10)
+	}
+	z01.PrintRune(rune(n%10 + '0'))
+}
+
+func printStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+}
+
+func main() {
+	points := &point{}
+	setPoint(points)
+
+	printStr("x = ")
+	printNbr(points.x)
+	printStr(", y = ")
+	printNbr(points.y)
+	z01.PrintRune('\n')
+}
