fa6ba537c8c76d29bb2965b73c2bd0b3c6fa1423
abassili <aggelosbassili@gmail.com>
2025-05-20 17:01:01 +0300

    sortwordarr

--- sortwordarr.go
+++ sortwordarr.go
@@ -1,7 +1,12 @@
 package student
 
-import "sort"
-
 func SortWordArr(a []string) {
-	sort.Strings(a)
+	n := len(a)
+	for i := 0; i < n-1; i++ {
+		for j := 0; j < n-i-1; j++ {
+			if a[j] > a[j+1] {
+				a[j], a[j+1] = a[j+1], a[j]
+			}
+		}
+	}
 }
