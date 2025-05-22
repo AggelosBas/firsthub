f74d97550e5b44e8285584036c41705e404c4fe4
abassili <aggelosbassili@gmail.com>
2025-05-22 16:25:19 +0300

    hackathon shoppingsummarycounter

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -1,16 +1,17 @@
 package student
 
-func Split(s string, sep string) []string {
-	var result []string
-	word := ""
-	for i := 0; i < len(s); i++ {
-		if string(s[i]) != sep {
-			word += string(s[i])
-		} else {
-			result = append(result, word)
-			word = ""
-		}
-	}
-	result = append(result, word)
-	return result
+func ShoppingSummaryCounter(str string) map[string]int {
+    counts := make(map[string]int)
+    word := ""
+    for i := 0; i <= len(str); i++ {
+        if i == len(str) || str[i] == ' ' {
+            if word != "" {
+                counts[word]++
+                word = ""
+            }
+        } else {
+            word += string(str[i])
+        }
+    }
+    return counts
 }
