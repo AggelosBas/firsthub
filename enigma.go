23ec0b70170c2bfce027486b83ecb1d92dcf067b
abassili <aggelosbassili@gmail.com>
2025-05-22 22:29:12 +0300

    hackathon enigma

---
+++ enigma.go
@@ -0,0 +1,15 @@
+package student
+
+func Enigma(a ***int, b *int, c *******int, d ****int) {
+	// Αποθήκευση προσωρινών τιμών
+	tempA := ***a
+	tempC := *******c
+	tempD := ****d
+	tempB := *b
+
+	// Εκχώρηση νέων τιμών
+	*******c = tempA
+	****d = tempC
+	*b = tempD
+	***a = tempB
+}
