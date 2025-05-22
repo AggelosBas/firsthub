d63d8756798942758a35deba28c34d635d689fd4
abassili <aggelosbassili@gmail.com>
2025-05-22 16:34:31 +0300

    hackathon shoppingsummarycounter

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -1,10 +1,14 @@
 package student
 
+func isSpace(c byte) bool {
+	return c == ' ' || c == '\t' || c == '\n' || c == '\r'
+}
+
 func ShoppingSummaryCounter(str string) map[string]int {
 	counts := make(map[string]int)
 	word := ""
 	for i := 0; i <= len(str); i++ {
-		if i == len(str) || str[i] == ' ' {
+		if i == len(str) || isSpace(str[i]) {
 			if word != "" {
 				counts[word]++
 				word = ""
