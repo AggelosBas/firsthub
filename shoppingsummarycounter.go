a301bdf5007ddfc41db158ceb6516908d7042e95
abassili <aggelosbassili@gmail.com>
2025-05-22 16:42:21 +0300

    hackathon shoppingsummarycounter

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -1,24 +1,20 @@
 package student
 
-// ShoppingSummaryCounter takes a string containing grocery items separated by spaces
-// and returns a map summarizing the count of each item.
 func ShoppingSummaryCounter(str string) map[string]int {
-    // Create a map to store item counts.
-    summary := make(map[string]int)
-
-    // Manually split the input string into words using only built-in functions.
-    word := ""
-    for i := 0; i < len(str); i++ {
-        if str[i] != ' ' {
-            word += string(str[i])
-        } else {
-            summary[word]++
-            word = ""
-        }
-    }
-    // Add the last word if there is one
-    summary[word]++
-
-    // Return the summary map.
-    return summary
+	summary := make(map[string]int)
+	word := ""
+	for i := 0; i < len(str); i++ {
+		if str[i] != ' ' {
+			word += string(str[i])
+		} else {
+			if word != "" {
+				summary[word]++
+				word = ""
+			}
+		}
+	}
+	if word != "" {
+		summary[word]++
+	}
+	return summary
 }
