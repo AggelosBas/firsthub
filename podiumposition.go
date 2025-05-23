dbb2d4add3e92465ab66978fe7cabac062dbd8ac
abassili <aggelosbassili@gmail.com>
2025-05-23 03:07:02 +0300

    podiumposition

--- podiumposition.go
+++ podiumposition.go
@@ -2,15 +2,19 @@ package student
 
 func PodiumPosition(podium [][]string) [][]string {
 	n := len(podium)
-	var result [][]string
-
-	for i := 0; i < n; i++ {
-		result = append(result, []string{})
-	}
+	result := makeFixedSlice(n)
 
 	for i := 0; i < n; i++ {
 		result[i] = podium[n-1-i]
 	}
 
 	return result
+}
+
+func makeFixedSlice(n int) [][]string {
+	var res [][]string
+	for i := 0; i < n; i++ {
+		res = append(res, []string{})
+	}
+	return res
 }
