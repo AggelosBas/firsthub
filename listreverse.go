77b82cae7dc9d39acf4f55b7a93a69ebffa71e26
abassili <aggelosbassili@gmail.com>
2025-05-26 12:19:39 +0300

    listreverse

---
+++ listreverse.go
@@ -0,0 +1,16 @@
+package student
+
+func ListReverse(l *List) {
+	var prev *NodeL = nil
+	current := l.Head
+	l.Tail = l.Head // η παλιά Head γίνεται Tail
+
+	for current != nil {
+		next := current.Next    // κρατάμε τον επόμενο
+		current.Next = prev     // αντιστρέφουμε τον δείκτη
+		prev = current          // ενημερώνουμε το προηγούμενο
+		current = next          // πάμε στον επόμενο
+	}
+
+	l.Head = prev // η τελευταία τιμή του prev είναι η νέα Head
+}
