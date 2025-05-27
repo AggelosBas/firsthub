5d949b0fffe358b78efc08a112e4bb0e77faf8b3
abassili <aggelosbassili@gmail.com>
2025-05-27 21:00:59 +0300

    list

--- sortlistinsert.go
+++ sortlistinsert.go
@@ -1,23 +1,20 @@
 package student
 
 func SortListInsert(l *NodeI, data_ref int) *NodeI {
-    newNode := &NodeI{Data: data_ref}
+	newNode := &NodeI{Data: data_ref}
 
-    // Περίπτωση: λίστα είναι άδεια ή το νέο στοιχείο είναι μικρότερο από την κεφαλή
-    if l == nil || data_ref < l.Data {
-        newNode.Next = l
-        return newNode
-    }
+	if l == nil || data_ref < l.Data {
+		newNode.Next = l
+		return newNode
+	}
 
-    current := l
-    // Βρίσκουμε τη θέση για εισαγωγή
-    for current.Next != nil && current.Next.Data < data_ref {
-        current = current.Next
-    }
+	current := l
+	for current.Next != nil && current.Next.Data < data_ref {
+		current = current.Next
+	}
 
-    // Εισάγουμε τον νέο κόμβο
-    newNode.Next = current.Next
-    current.Next = newNode
+	newNode.Next = current.Next
+	current.Next = newNode
 
-    return l
+	return l
 }
