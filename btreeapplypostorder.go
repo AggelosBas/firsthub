3399c31408f394d8379ec0df5ef687175aee9df9
abassili <aggelosbassili@gmail.com>
2025-05-27 14:23:50 +0300

    btree

--- btreeapplypostorder.go
+++ btreeapplypostorder.go
@@ -4,7 +4,7 @@ func BTreeApplyPostorder(root *TreeNode, f func(...interface{}) (int, error)) {
 	if root == nil {
 		return
 	}
-	BTreeApplyPostorder(root.Left, f)   // 1. Επισκέπτεται το αριστερό παιδί
-	BTreeApplyPostorder(root.Right, f)  // 2. Επισκέπτεται το δεξί παιδί
-	f(root.Data) // 3. Εφαρμόζει τη συνάρτηση στον τρέχοντα κόμβο
+	BTreeApplyPostorder(root.Left, f)
+	BTreeApplyPostorder(root.Right, f)
+	f(root.Data)
 }
